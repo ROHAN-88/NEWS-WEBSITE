c855748ac503780ff77d143bd9d86e7c551380cf
Rohan <therohanshrestha6655@gmail.com>
2023-04-12 18:57:12 +0545

    change in date 12/04/2023

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # NEWS-WEBSITE
 
 this file contains the sources code for a news website prototype.
+
+
+
+
+
+trun the side bar into link .
--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,43 @@ const subbutton = document.querySelector("#subscrib");
 function sub(){
 
     alert("you have Subscribe");
-    subbutton.style.background="rgb(189, 8, 8)";
+    subbutton.style.background="rgb(204, 92, 92)";
     subbutton.style.color="black";
 
     subbutton.innerHTML = "SUBSCIBED";
-    
+  
 }
+
+
+
+// owl crousel ----------------------
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
